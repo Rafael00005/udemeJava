ea0deafcf0e9cde62b5fa3aef21d37f457b2aea4
Rafael00005 <145172569+Rafael00005@users.noreply.github.com>
2024-01-01 22:32:23 +0000

    p

--- Teste.java
+++ Teste.java
@@ -1,9 +1,6 @@
-
-
-class Main {
-  public static void main(String[] args) {
+class Test{
+  public static void main(String[] args){
     System.out.println("Hello world!");
     System.out.println("Rafael");
   }
-
 }
